0e235e1934daf08ae75a3b9c67f89c2d635adf47
Siim Veske <siim.veske@gmail.com>
2022-01-29 21:44:11 +0200

    solve between_markers

---
+++ 016_between_markers.py
@@ -0,0 +1,18 @@
+def between_markers(text: str, begin: str, end: str) -> str:
+    """
+        returns substring between two given markers
+    """
+    start = text.index(begin)+1
+    stop = text.index(end)
+
+    return text[start:stop]
+
+
+if __name__ == '__main__':
+
+    assert between_markers('What is >apple<', '>', '<') == "apple"
+    assert between_markers('What is [apple]', '[', ']') == "apple"
+    assert between_markers('What is ><', '>', '<') == ""
+    assert between_markers('>apple<', '>', '<') == "apple"
+
+    print('OK')
