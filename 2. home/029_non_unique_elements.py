9ada4ebfbf72d474679d7577e043fed0b2f3b6a9
Siim Veske <siim.veske@gmail.com>
2022-02-17 09:42:42 +0200

    solve Non-unique Elements

---
+++ 2. home/029_non_unique_elements.py
@@ -0,0 +1,24 @@
+"""
+You are given a non-empty list of integers (X). For this task, you should return a list consisting of only the non-unique elements in this list. To do so you will need to remove all unique elements (elements which are contained in a given list only once). When solving this task, do not change the order of the list. Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
+"""
+
+
+def checkio(data: list) -> list:
+    memo = {}
+    result = []
+    for item in data:
+        if item not in memo:
+            memo[item] = data.count(item)
+        if memo[item] > 1:
+            result.append(item)
+    return result
+
+
+if __name__ == '__main__':
+
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+    assert checkio([1, 2, 3, 4, 5]) == []
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+
+    print("OK")
