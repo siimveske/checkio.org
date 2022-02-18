59fe57d44ef50d724852470485e4a9073d30877a
Siim Veske <siim.veske@gmail.com>
2022-02-18 22:28:30 +0200

    refactor safe_pawns

--- 2. home/033_pawn_brotherhood.py
+++ 2. home/033_pawn_brotherhood.py
@@ -4,19 +4,16 @@ Sort the given iterable so that its elements end up in the decreasing frequency 
 
 
 def safe_pawns(pawns: set) -> int:
-    num_to_letter = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h'}
-    letter_to_num = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
     cnt = 0
-
     for pawn in pawns:
 
         row = int(pawn[1])
-        col = letter_to_num[pawn[0]]
+        col = ord(pawn[0])
 
-        location1 = f'{num_to_letter.get(col - 1, -1)}{row - 1}'
-        location2 = f'{num_to_letter.get(col + 1, -1)}{row - 1}'
+        defender1 = chr(col - 1) + str(row - 1)
+        defender2 = chr(col + 1) + str(row - 1)
 
-        if location1 in pawns or location2 in pawns:
+        if defender1 in pawns or defender2 in pawns:
             cnt += 1
 
     return cnt
