412ce5acd2daa3930a305890a4bd943752585389
Siim Veske <siim.veske@gmail.com>
2022-02-19 21:23:04 +0200

    solve "Sort Except Zero"

---
+++ 3. o'reilly/048_sort_except_zero.py
@@ -0,0 +1,22 @@
+from typing import Iterable
+
+
+def except_zero(items: list) -> Iterable:
+    numbers = [i for i in items if i != 0]
+    numbers.sort(reverse=True)
+    result = []
+    for i in items:
+        if i == 0:
+            result.append(0)
+        else:
+            result.append(numbers.pop())
+    return result
+
+
+if __name__ == '__main__':
+    assert list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])) == [1, 3, 0, 0, 4, 4, 5, 0, 7]
+    assert list(except_zero([0, 2, 3, 1, 0, 4, 5])) == [0, 1, 2, 3, 0, 4, 5]
+    assert list(except_zero([0, 0, 0, 1, 0])) == [0, 0, 0, 1, 0]
+    assert list(except_zero([4, 5, 3, 1, 1])) == [1, 1, 3, 4, 5]
+    assert list(except_zero([0, 0])) == [0, 0]
+    print("OK")
