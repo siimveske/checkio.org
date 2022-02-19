c9dfde9a2d5c9f11ca0fa91995677c63e93333fe
Siim Veske <siim.veske@gmail.com>
2022-02-19 17:12:16 +0200

    solve "Lightbulb Start Watching"

---
+++ 3. o'reilly/043_lightbulb_start_watching.py
@@ -0,0 +1,114 @@
+from datetime import datetime
+from typing import List, Optional
+
+
+def sum_light(els: List[datetime], start_watching: Optional[datetime] = None) -> int:
+    """
+    how long the light bulb has been turned on
+    """
+    result = 0
+    i, j = 0, 1
+    while j < len(els):
+        start = els[i]
+        stop = els[j]
+        if start_watching:
+            if start <= start_watching <= stop:
+                start = start_watching
+            if start_watching <= start:
+                result += (stop - start).total_seconds()
+        else:
+            result += (stop - start).total_seconds()
+
+        i += 2
+        j += 2
+
+    return int(result)
+
+
+if __name__ == '__main__':
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ],
+            datetime(2015, 1, 12, 10, 0, 5),
+        ) == 5
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ],
+            datetime(2015, 1, 12, 10, 0, 0),
+        ) == 10
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 11, 0, 0),
+        ) == 610
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 11, 0, 10),
+        ) == 600
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 10, 10, 0),
+        ) == 620
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+                datetime(2015, 1, 12, 11, 10, 11),
+                datetime(2015, 1, 12, 12, 10, 11),
+            ],
+            datetime(2015, 1, 12, 12, 10, 11),
+        ) == 0
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+                datetime(2015, 1, 12, 11, 10, 11),
+                datetime(2015, 1, 12, 12, 10, 11),
+            ],
+            datetime(2015, 1, 12, 12, 9, 11),
+        ) == 60
+    )
+
+    print("OK")
