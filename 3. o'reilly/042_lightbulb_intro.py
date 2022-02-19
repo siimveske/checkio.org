d41d73b4418b0a330bd9aa7fc57deb3b17e3df3a
Siim Veske <siim.veske@gmail.com>
2022-02-19 16:40:15 +0200

    solve lightbulb intro

---
+++ 3. o'reilly/042_lightbulb_intro.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from typing import List
+
+
+def sum_light(els: List[datetime]) -> int:
+    """
+        how long the light bulb has been turned on
+    """
+    result = 0
+    i, j = 0, 1
+    while j < len(els):
+        result += (els[j] - els[i]).total_seconds()
+        i += 2
+        j += 2
+
+    return int(result)
+
+
+if __name__ == '__main__':
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 10, 10),
+    ]) == 610
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 10, 10),
+        datetime(2015, 1, 12, 11, 0, 0),
+        datetime(2015, 1, 12, 11, 10, 10),
+    ]) == 1220
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 10, 10),
+        datetime(2015, 1, 12, 11, 0, 0),
+        datetime(2015, 1, 12, 11, 10, 10),
+        datetime(2015, 1, 12, 11, 10, 10),
+        datetime(2015, 1, 12, 12, 10, 10),
+    ]) == 4820
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 0, 1),
+    ]) == 1
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 0, 10),
+        datetime(2015, 1, 12, 11, 0, 0),
+        datetime(2015, 1, 13, 11, 0, 0),
+    ]) == 86410
+
+    print("OK")
