0a2a5bbe45fded76f381718a23028cabab8a1e5f
Siim Veske <siim.veske@gmail.com>
2022-02-19 09:31:18 +0200

    add all_the_same_short

--- 2. home/036_all_the_same.py
+++ 2. home/036_all_the_same.py
@@ -9,6 +9,13 @@ def all_the_same(elements: List[Any]) -> bool:
     return True
 
 
+def all_the_same_short(elements: List[Any]) -> bool:
+    # elements = [X1,X2,X3,X4,X5]
+    # elements[:-1] # [X1,X2,X3,X4]
+    # elements[1:] #  [X2,X3,X4,X5]
+    return elements[:-1] == elements[1:]
+
+
 if __name__ == '__main__':
 
     assert all_the_same([1, 1, 1]) == True
