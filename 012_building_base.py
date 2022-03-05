0744a3b58c9dea0185a6ac161ffab0a7cf51058a
Siim Veske <siim.veske@gmail.com>
2022-03-05 12:48:45 +0200

    solve "Building Base"

--- 012_building_base.py
+++ 012_building_base.py
@@ -15,28 +15,27 @@ class Building:
         }
 
     def area(self):
-        raise NotImplementedError
+        return self.width_NS * self.width_WE
 
     def volume(self):
-        raise NotImplementedError
+        return self.area() * self.height
 
     def __repr__(self):
-        raise NotImplementedError
+        s, w, we, ns, h = self.south, self.west, self.width_WE, self.width_NS, self.height
+        return f"Building({s}, {w}, {we}, {ns}, {h})"
 
 
 if __name__ == '__main__':
     def json_dict(d):
         return dict((k, list(v)) for k, v in d.items())
 
-    b = json_dict(Building(1, 2, 2, 2).corners())
-    print(b)
-    # b = Building(1, 2, 2, 3)
-    # b2 = Building(1, 2, 2, 3, 5)
-    # assert json_dict(b.corners()) == {'north-east': [4, 4], 'south-east': [1, 4],
-    #                                   'south-west': [1, 2], 'north-west': [4, 2]}, "Corners"
-    # assert b.area() == 6, "Area"
-    # assert b.volume() == 60, "Volume"
-    # assert b2.volume() == 30, "Volume2"
-    # assert str(b) == "Building(1, 2, 2, 3, 10)", "String"
+    b = Building(1, 2, 2, 3)
+    b2 = Building(1, 2, 2, 3, 5)
+    assert json_dict(b.corners()) == {'north-east': [4, 4], 'south-east': [1, 4],
+                                      'south-west': [1, 2], 'north-west': [4, 2]}, "Corners"
+    assert b.area() == 6, "Area"
+    assert b.volume() == 60, "Volume"
+    assert b2.volume() == 30, "Volume2"
+    assert str(b) == "Building(1, 2, 2, 3, 10)", "String"
 
     print("OK")
