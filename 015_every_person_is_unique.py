c39070446e48e2d9e1663b2d76a550cc6ac3008b
Siim Veske <siim.veske@gmail.com>
2022-03-06 21:58:35 +0200

    refactor

--- 015_every_person_is_unique.py
+++ 015_every_person_is_unique.py
@@ -1,37 +1,18 @@
-from datetime import datetime
-
-
 class Person:
     def __init__(self, first_name, last_name, birth_date, job, working_years, salary, country, city, gender="unknown"):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.birth_date = birth_date
-        self.job = job
-        self.working_years = working_years
-        self.salary = salary
-        self.country = country
-        self.city = city
-        self.gender = gender
+        self.__dict__.update(locals())
+        del self.__dict__['self']
 
     def name(self):
         return f"{self.first_name} {self.last_name}"
 
     def age(self):
-        birth_date = datetime.strptime(self.birth_date, "%d.%m.%Y")
-        cur_date = datetime.strptime("01.01.2018", "%d.%m.%Y")
-        delta = cur_date - birth_date
-        age = int(delta.days // 365.2425)
-        return age
+        return 2017 - int(self.birth_date.split('.')[2])
 
     def work(self):
-        if self.gender == "male":
-            out = f"He is a {self.job}"
-        elif self.gender == "female":
-            out = f"She is a {self.job}"
-        else:
-            out = f"Is a {self.job}"
-
-        return out
+        return {'unknown': 'Is a ',
+                'male': 'He is a ',
+                'female': 'She is a '}[self.gender] + self.job
 
     def money(self):
         amount = int(self.working_years * self.salary * 12)
