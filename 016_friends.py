dfe6ffa3a1fe0c13770d61eb988781ab8ec05aa8
Siim Veske <siim.veske@gmail.com>
2022-03-07 10:51:04 +0200

    solve friends

--- 016_friends.py
+++ 016_friends.py
@@ -1,18 +1,39 @@
+from collections import defaultdict
+
+
 class Friends:
+
     def __init__(self, connections):
-        raise NotImplementedError
+        self.graph = defaultdict(set)
+        for connection in connections:
+            self.add(connection)
 
     def add(self, connection):
-        raise NotImplementedError
+        a, b = connection
+        if is_new := b not in self.graph[a]:
+            self.graph[a].add(b)
+            self.graph[b].add(a)
+        return is_new
 
     def remove(self, connection):
-        raise NotImplementedError
+        a, b = connection
+        if connection_exists := a in self.graph and b in self.graph[a]:
+            self.graph[a].remove(b)
+            if not self.graph[a]:
+                self.graph.pop(a)
+            self.graph[b].remove(a)
+            if not self.graph[b]:
+                self.graph.pop(b)
+
+        return connection_exists
 
     def names(self):
-        raise NotImplementedError
+        return set(self.graph.keys())
 
     def connected(self, name):
-        raise NotImplementedError
+        if name not in self.graph:
+            return set()
+        return self.graph[name]
 
 
 if __name__ == '__main__':
@@ -27,5 +48,13 @@ if __name__ == '__main__':
     assert letter_friends.names() == {"a", "b", "c"}, "Names"
     assert letter_friends.connected("d") == set(), "Non connected name"
     assert letter_friends.connected("a") == {"b", "c"}, "Connected name"
+
+    # 3. Remove/0
+    f = Friends([{"1", "2"}, {"3", "1"}])
+    assert f.remove({"2", "4"}) == False
+
+    # 5. Connected/0
+    f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))
+    assert f.connected("nikola") == {"sophia"}
 
     print("OK")
