4449f6eec0bafd64d42d504c6ded16f588e7a226
Siim Veske <siim.veske@gmail.com>
2022-03-07 18:09:14 +0200

    implement write,delete,send

--- 018_hacker_language.py
+++ 018_hacker_language.py
@@ -1,12 +1,24 @@
 class HackerLanguage:
+    def __init__(self):
+        self.text = []
+
     def write(self, text):
-        pass
+        for i in text:
+            self.text.append(i)
 
     def delete(self, idx):
-        pass
+        self.text = self.text[0:-idx]
 
     def send(self):
-        pass
+        buffer = []
+        for i in self.text:
+            if i == ' ':
+                buffer.append('1000000')
+            elif i.isalpha():
+                buffer.append(bin(ord(i))[2:])
+            else:
+                buffer.append(i)
+        return ''.join(buffer)
 
     def read(self, text):
         pass
@@ -18,9 +30,16 @@ if __name__ == '__main__':
     message_1.write("secrit")
     message_1.delete(2)
     message_1.write("et")
-    message_2 = HackerLanguage()
 
     assert message_1.send() == "111001111001011100011111001011001011110100"
+
+    message_1 = HackerLanguage()
+    message_1.write('Remember: 21.07.2018 at 11:11AM')
+    message_1.delete(2)
+    message_1.write('PM')
+    message_1.send() == '10100101100101110110111001011101101110001011001011110010:100000021.07.2018100000011000011110100100000011:1110100001001101'
+
+    message_2 = HackerLanguage()
     assert message_2.read("11001011101101110000111010011101100") == "email"
 
     print("OK")
