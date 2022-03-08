ebc811936ff063f1d40b463b091c2de38f3e5eb3
Siim Veske <siim.veske@gmail.com>
2022-03-08 16:15:51 +0200

    solve "Voice TV Control"

--- 023_voice_tv_control.py
+++ 023_voice_tv_control.py
@@ -1,5 +1,36 @@
 class VoiceCommand:
-    pass
+    def __init__(self, channels) -> None:
+        self.channels = channels
+        self.active_channel = 0
+
+    def first_channel(self):
+        self.active_channel = 0
+        return self.channels[self.active_channel]
+
+    def last_channel(self):
+        self.active_channel = len(self.channels) - 1
+        return self.channels[self.active_channel]
+
+    def turn_channel(self, n):
+        self.active_channel = n - 1
+        return self.channels[self.active_channel]
+
+    def next_channel(self):
+        self.active_channel = (self.active_channel + 1) % len(self.channels)
+        return self.channels[self.active_channel]
+
+    def previous_channel(self):
+        self.active_channel = (self.active_channel - 1) % len(self.channels)
+        return self.channels[self.active_channel]
+
+    def current_channel(self):
+        return self.channels[self.active_channel]
+
+    def is_exist(self, channel):
+        if type(channel) is int:
+            return "Yes" if 0 < channel <= len(self.channels) else "No"
+        else:
+            return "Yes" if channel in self.channels else "No"
 
 
 if __name__ == '__main__':
