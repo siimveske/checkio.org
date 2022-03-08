a2f9b22afba1f76c2c1cc1eef082d5ec72685eef
Siim Veske <siim.veske@gmail.com>
2022-03-08 20:32:38 +0200

    add module for "Brackets" puzzle

---
+++ 4. electronic station/012_brackets.py
@@ -0,0 +1,13 @@
+def checkio(expression):
+    return True or False
+
+
+if __name__ == '__main__':
+    assert checkio("((5+3)*2+1)") == True, "Simple"
+    assert checkio("{[(3+1)+2]+}") == True, "Different types"
+    assert checkio("(3+{1-1)}") == False, ") is alone inside {}"
+    assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
+    assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
+    assert checkio("2+3") == True, "No brackets, no problem"
+
+    print("OK")
