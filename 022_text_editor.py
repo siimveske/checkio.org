9c9f1c1f7849b748e27df9aa9a30e43a06848399
Siim Veske <siim.veske@gmail.com>
2022-03-08 11:55:00 +0200

    add module for "Text Editor"

---
+++ 022_text_editor.py
@@ -0,0 +1,25 @@
+class Text:
+    pass
+
+
+class SavedText:
+    pass
+
+
+if __name__ == '__main__':
+
+    text = Text()
+    saver = SavedText()
+
+    text.write("At the very beginning ")
+    saver.save_text(text)
+    text.set_font("Arial")
+    saver.save_text(text)
+    text.write("there was nothing.")
+
+    assert text.show() == "[Arial]At the very beginning there was nothing.[Arial]"
+
+    text.restore(saver.get_version(0))
+    assert text.show() == "At the very beginning "
+
+    print("OK")
