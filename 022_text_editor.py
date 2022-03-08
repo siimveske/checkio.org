576b2b60b9f583c2f48f247552385735b948c4b9
Siim Veske <siim.veske@gmail.com>
2022-03-08 12:32:23 +0200

    solve "Text Editor"

--- 022_text_editor.py
+++ 022_text_editor.py
@@ -1,9 +1,41 @@
+import copy
+
+
 class Text:
-    pass
+    def __init__(self):
+        self.text = []
+        self.font = ""
+
+    def write(self, text: str):
+        self.text.append(text)
+
+    def set_font(self, font: str):
+        self.font = font
+
+    def show(self):
+        if self.font:
+            return f"[{self.font}]{''.join(self.text)}[{self.font}]"
+        else:
+            return ''.join(self.text)
+
+    def restore(self, commit: tuple):
+        font, text = commit
+        self.font = font
+        self.text = text
 
 
 class SavedText:
-    pass
+    def __init__(self):
+        self.repo = dict()
+        self.version = 0
+
+    def save_text(self, text: Text):
+        self.repo[self.version] = (text.font, copy.deepcopy(text.text))
+        self.version += 1
+
+    def get_version(self, number: int):
+        font, text = self.repo.get(number)
+        return (font, copy.deepcopy(text))
 
 
 if __name__ == '__main__':
