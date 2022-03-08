9aa8c832f5e34be7d75a58f45d493c93d1285325
Siim Veske <siim.veske@gmail.com>
2022-03-08 16:30:37 +0200

    add module for "Remove Accents"

---
+++ 5. scientific expedition/002_remove_accents.py
@@ -0,0 +1,8 @@
+def checkio(in_string):
+    return in_string
+
+
+if __name__ == '__main__':
+    assert checkio(u"préfèrent") == u"preferent"
+    assert checkio(u"loài trăn lớn") == u"loai tran lon"
+    print('Done')
