452bf059b746ee8a51546b46de29f09ba0b3bcac
Siim Veske <siim.veske@gmail.com>
2022-03-09 11:11:57 +0200

    refactor anagrams

--- 4. electronic station/017_verify_anagrams.py
+++ 4. electronic station/017_verify_anagrams.py
@@ -1,24 +1,13 @@
 '''
 https://py.checkio.org/en/mission/verify-anagrams/
 '''
-from collections import defaultdict
 
 
 def verify_anagrams(a: str, b: str) -> bool:
-    s1 = a.replace(' ', '').lower()
-    s2 = b.replace(' ', '').lower()
+    s1 = sorted(a.replace(' ', '').lower())
+    s2 = sorted(b.replace(' ', '').lower())
 
-    if len(s1) != len(s2):
-        return False
-
-    s1_letter_count = defaultdict(int)
-    s2_letter_count = defaultdict(int)
-
-    for idx in range(len(s1)):
-        s1_letter_count[s1[idx]] += 1
-        s2_letter_count[s2[idx]] += 1
-
-    return s1_letter_count == s2_letter_count
+    return s1 == s2
 
 
 if __name__ == '__main__':
