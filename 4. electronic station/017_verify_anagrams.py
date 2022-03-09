7ec27dc58aaa6965da9dbbd380c3caef0f58fd96
Siim Veske <siim.veske@gmail.com>
2022-03-09 11:08:23 +0200

    solve "Verify Anagrams"

--- 4. electronic station/017_verify_anagrams.py
+++ 4. electronic station/017_verify_anagrams.py
@@ -1,10 +1,24 @@
 '''
 https://py.checkio.org/en/mission/verify-anagrams/
 '''
+from collections import defaultdict
 
 
-def verify_anagrams(a, b):
-    return None
+def verify_anagrams(a: str, b: str) -> bool:
+    s1 = a.replace(' ', '').lower()
+    s2 = b.replace(' ', '').lower()
+
+    if len(s1) != len(s2):
+        return False
+
+    s1_letter_count = defaultdict(int)
+    s2_letter_count = defaultdict(int)
+
+    for idx in range(len(s1)):
+        s1_letter_count[s1[idx]] += 1
+        s2_letter_count[s2[idx]] += 1
+
+    return s1_letter_count == s2_letter_count
 
 
 if __name__ == '__main__':
