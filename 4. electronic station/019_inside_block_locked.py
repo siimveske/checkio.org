1b8fb209c482ba39be2382bfd9f4a89b7af39d9c
Siim Veske <siim.veske@gmail.com>
2022-03-09 21:54:11 +0200

    solve "Inside Block"

--- 4. electronic station/019_inside_block_locked.py
+++ 4. electronic station/019_inside_block_locked.py
@@ -5,11 +5,11 @@ https://stackoverflow.com/questions/36399381/whats-the-fastest-way-of-checking-i
 from typing import Tuple
 
 
-def touches(polygon, point):
-    return point in polygon
-
-
 def is_inside(polygon: Tuple[Tuple[int, int], ...], point: Tuple[int, int]) -> bool:
+
+    if point in polygon:
+        return True
+
     x, y = point
 
     n = len(polygon)
@@ -18,6 +18,17 @@ def is_inside(polygon: Tuple[Tuple[int, int], ...], point: Tuple[int, int]) -> b
     p1x, p1y = polygon[0]
     for i in range(n + 1):
         p2x, p2y = polygon[i % n]
+
+        # Test if Point is on a vertical (y) line
+        if x == p1x and x == p2x:
+            if (min(p1y, p2y) <= y <= max(p1y, p2y)):
+                return True
+
+        # Test if Point is on a horizontal (x) line
+        if y == p1y and y == p2y:
+            if (min(p1x, p2x) <= x <= max(p1x, p2x)):
+                return True
+
         if y > min(p1y, p2y):
             if y <= max(p1y, p2y):
                 if x <= max(p1x, p2x):
@@ -44,6 +55,9 @@ if __name__ == '__main__':
     assert is_inside([[3, 4], [4, 2], [2, 1], [1, 3]], [2, 2]) == True
 
     # Extra/3
-    is_inside([[1, 1], [1, 3], [3, 3], [3, 1]], [1, 1]) == True
+    assert is_inside([[1, 1], [1, 3], [3, 3], [3, 1]], [1, 1]) == True
+
+    # Extra/13
+    assert is_inside([[0, 0], [0, 2], [2, 2], [2, 0]], [1, 0]) == True
 
     print("OK")
