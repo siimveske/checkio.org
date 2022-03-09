8c845d10943e50c560f5bdde1f8e55ae58419c77
Siim Veske <siim.veske@gmail.com>
2022-03-09 10:15:21 +0200

    solve "Mathematically Lucky Tickets"

--- 4. electronic station/015_mathematically_lucky_tickets.py
+++ 4. electronic station/015_mathematically_lucky_tickets.py
@@ -1,5 +1,17 @@
+from itertools import product
+
+
+def possible_results(data):
+    yield int(data)
+    for i in range(1, len(data)):
+        for (x, y) in product(possible_results(data[:i]), possible_results(data[i:])):
+            yield from (x + y, x - y, x * y)
+            if y:
+                yield x / y
+
+
 def checkio(data):
-    return True or False
+    return True if 100 not in possible_results(data) else False
 
 
 if __name__ == '__main__':
@@ -7,3 +19,5 @@ if __name__ == '__main__':
     assert checkio('707409') == True, "You can not transform it to 100"
     assert checkio('595347') == False, "(5 + ((9 / (3 / 34)) - 7)) = 100"
     assert checkio('271353') == False, "(2 - (7 * (((1 / 3) - 5) * 3))) = 100"
+
+    print("OK")
