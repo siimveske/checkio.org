05121f4d428922d591d5667dd3ceb9a890f1cfc0
Siim Veske <siim.veske@gmail.com>
2022-03-10 20:46:43 +0200

    solve "How Deep"

---
+++ 3. o'reilly/051_how_deep.py
@@ -0,0 +1,25 @@
+'''
+https://py.checkio.org/en/mission/how-deep/
+'''
+
+
+def how_deep(structure):
+    depth = 1
+    for i in structure:
+        if type(i) is tuple:
+            d = 1 + how_deep(i)
+            if d > depth:
+                depth = d
+    return depth
+
+
+if __name__ == '__main__':
+    assert how_deep((1, 2, 3)) == 1
+    assert how_deep((1, 2, (3,))) == 2
+    assert how_deep((1, 2, (3, (4,)))) == 3
+    assert how_deep(()) == 1
+    assert how_deep(((),)) == 2
+    assert how_deep((((),),)) == 3
+    assert how_deep((1, (2,), (3,))) == 2
+    assert how_deep((1, ((),), (3,))) == 3
+    print("OK")
