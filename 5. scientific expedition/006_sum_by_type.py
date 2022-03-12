264be297dc7766779eeaca4b5046dad9794efcca
Siim Veske <siim.veske@gmail.com>
2022-03-12 10:41:50 +0200

    solve "Sum by Type"

---
+++ 5. scientific expedition/006_sum_by_type.py
@@ -0,0 +1,19 @@
+from typing import Tuple
+
+
+def sum_by_types(items: list) -> Tuple[str, int]:
+    result = {type(''): '', type(0): 0}
+    for item in items:
+        result[type(item)] += item
+    return tuple(result.values())
+
+
+if __name__ == "__main__":
+    assert sum_by_types([]) == ("", 0)
+    assert sum_by_types([1, 2, 3]) == ("", 6)
+    assert sum_by_types(["1", 2, 3]) == ("1", 5)
+    assert sum_by_types(["1", "2", 3]) == ("12", 3)
+    assert sum_by_types(["1", "2", "3"]) == ("123", 0)
+    assert sum_by_types(["size", 12, "in", 45, 0]) == ("sizein", 57)
+
+    print("OK")
