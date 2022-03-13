a1787d80f8408bff715e6c0817644e900b0a18c8
Siim Veske <siim.veske@gmail.com>
2022-03-13 11:03:58 +0200

    add module for "Bird Language"

---
+++ 5. scientific expedition/007_bird_language.py
@@ -0,0 +1,16 @@
+'''
+https://py.checkio.org/en/mission/bird-language/
+'''
+
+
+def translate(text: str) -> str:
+    return None
+
+
+if __name__ == "__main__":
+    assert translate("hieeelalaooo") == "hello"
+    assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin"
+    assert translate("aaa bo cy da eee fe") == "a b c d e f"
+    assert translate("sooooso aaaaaaaaa") == "sos aaa"
+
+    print("OK")
