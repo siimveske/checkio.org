c6f7045a94554d5e0e2162f9332d3336412eb0e0
Siim Veske <siim.veske@gmail.com>
2022-03-16 20:07:19 +0200

    cleanup

--- 3. o'reilly/065_loading_cargo.py
+++ 3. o'reilly/065_loading_cargo.py
@@ -3,10 +3,10 @@ def checkio(data):
 
 
 if __name__ == '__main__':
-    assert checkio([10, 10]) == 0, "1st example"
-    assert checkio([10]) == 10, "2nd example"
-    assert checkio([5, 8, 13, 27, 14]) == 3, "3rd example"
-    assert checkio([5, 5, 6, 5]) == 1, "4th example"
-    assert checkio([12, 30, 30, 32, 42, 49]) == 9, "5th example"
-    assert checkio([1, 1, 1, 3]) == 0, "6th example"
+    assert checkio([10, 10]) == 0
+    assert checkio([10]) == 10
+    assert checkio([5, 8, 13, 27, 14]) == 3
+    assert checkio([5, 5, 6, 5]) == 1
+    assert checkio([12, 30, 30, 32, 42, 49]) == 9
+    assert checkio([1, 1, 1, 3]) == 0
     print('OK')
