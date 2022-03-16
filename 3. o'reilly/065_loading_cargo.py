02afe47c5af23dd7c5cd6dc6048ef940c5fe8611
Siim Veske <siim.veske@gmail.com>
2022-03-16 20:06:12 +0200

    add "Loading Cargo" module

---
+++ 3. o'reilly/065_loading_cargo.py
@@ -0,0 +1,12 @@
+def checkio(data):
+    return 0
+
+
+if __name__ == '__main__':
+    assert checkio([10, 10]) == 0, "1st example"
+    assert checkio([10]) == 10, "2nd example"
+    assert checkio([5, 8, 13, 27, 14]) == 3, "3rd example"
+    assert checkio([5, 5, 6, 5]) == 1, "4th example"
+    assert checkio([12, 30, 30, 32, 42, 49]) == 9, "5th example"
+    assert checkio([1, 1, 1, 3]) == 0, "6th example"
+    print('OK')
