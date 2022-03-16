98fe1e4622e752c7c97167c1a17bfcc9b5e0ac8f
Siim Veske <siim.veske@gmail.com>
2022-03-16 23:42:49 +0200

    save WIP

--- 3. o'reilly/065_loading_cargo.py
+++ 3. o'reilly/065_loading_cargo.py
@@ -1,5 +1,18 @@
-def checkio(data):
-    return 0
+'''
+https://py.checkio.org/en/mission/loading-cargo/
+'''
+
+
+def checkio(data: list):
+    '''Karmarkar-Karp algorithm'''
+    data.sort()
+    while len(data) > 1:
+        a = data.pop()
+        b = data.pop()
+        data.append(abs(a - b))
+        data.sort()
+
+    return data[0]
 
 
 if __name__ == '__main__':
@@ -9,4 +22,7 @@ if __name__ == '__main__':
     assert checkio([5, 5, 6, 5]) == 1
     assert checkio([12, 30, 30, 32, 42, 49]) == 9
     assert checkio([1, 1, 1, 3]) == 0
+
+    # Basics/6
+    assert checkio([9, 9, 7, 6, 5]) == 0
     print('OK')
