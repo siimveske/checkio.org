4f0ed35ef76a34b8ab41005ccbf8703e75aaa72e
Siim Veske <siim.veske@gmail.com>
2022-03-18 01:29:12 +0200

    save geeksforgeek solution for "Loading Cargo"

--- 3. o'reilly/065_loading_cargo.py
+++ 3. o'reilly/065_loading_cargo.py
@@ -1,18 +1,53 @@
 '''
 https://py.checkio.org/en/mission/loading-cargo/
+https://www.geeksforgeeks.org/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum/
 '''
 
 
 def checkio(data: list):
-    '''Karmarkar-Karp algorithm'''
-    data.sort()
-    while len(data) > 1:
-        a = data.pop()
-        b = data.pop()
-        data.append(abs(a - b))
-        data.sort()
+    n = len(data)
+    result = findMin(data, n)
+    return result
 
-    return data[0]
+
+def findMinRec(arr, i, sumCalculated,
+               sumTotal):
+
+    # If we have reached last element.
+    # Sum of one subset is sumCalculated,
+    # sum of other subset is sumTotal-
+    # sumCalculated.  Return absolute
+    # difference of two sums.
+    if (i == 0):
+        return abs((sumTotal - sumCalculated) -
+                   sumCalculated)
+
+    # For every item arr[i], we have two choices
+    # (1) We do not include it first set
+    # (2) We include it in first set
+    # We return minimum of two choices
+    return min(findMinRec(arr, i - 1,
+                          sumCalculated + arr[i - 1],
+                          sumTotal),
+               findMinRec(arr, i - 1,
+                          sumCalculated, sumTotal))
+
+# Returns minimum possible
+# difference between sums
+# of two subsets
+
+
+def findMin(arr, n):
+
+    # Compute total sum
+    # of elements
+    sumTotal = 0
+    for i in range(n):
+        sumTotal += arr[i]
+
+    # Compute result using
+    # recursive function
+    return findMinRec(arr, n, 0, sumTotal)
 
 
 if __name__ == '__main__':
