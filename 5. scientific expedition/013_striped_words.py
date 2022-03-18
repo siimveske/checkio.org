ac15a4c179852eaebfd2a2376a87b2eb89cb4a46
Siim Veske <siim.veske@gmail.com>
2022-03-18 08:14:09 +0200

    add "Striped Words" module

---
+++ 5. scientific expedition/013_striped_words.py
@@ -0,0 +1,16 @@
+'''
+https://py.checkio.org/en/mission/striped-words/
+'''
+
+
+def checkio(line: str) -> str:
+    # your code here
+    return ''
+
+
+if __name__ == '__main__':
+    assert checkio('My name is ...') == 3
+    assert checkio('Hello world') == 0
+    assert checkio('A quantity of striped words.') == 1
+    assert checkio('Dog,cat,mouse,bird.Human.') == 3
+    print("OK")
