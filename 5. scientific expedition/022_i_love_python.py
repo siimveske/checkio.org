ab6a23baca2cf62d420d88e784b09e738e382761
Siim Veske <siim.veske@gmail.com>
2022-03-19 22:30:45 +0200

    add "I Love Python!" module

---
+++ 5. scientific expedition/022_i_love_python.py
@@ -0,0 +1,11 @@
+'''
+https://py.checkio.org/en/mission/i-love-python/
+'''
+
+
+def i_love_python():
+    return "I love Python!"
+
+
+if __name__ == '__main__':
+    assert i_love_python() == "I love Python!"
