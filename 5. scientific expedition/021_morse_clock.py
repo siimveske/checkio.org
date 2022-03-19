9b4fe1a627da182a5cc9dfcac3bdf4ba01cee556
Siim Veske <siim.veske@gmail.com>
2022-03-19 20:47:12 +0200

    refactor

--- 5. scientific expedition/021_morse_clock.py
+++ 5. scientific expedition/021_morse_clock.py
@@ -1,21 +1,14 @@
+'''
+https://py.checkio.org/en/mission/morse-clock/
+'''
+
+
 def checkio(time_string: str) -> str:
-    hhmmss = [f'{i:0>2}' for i in time_string.split(':')]
-
-    # convert hour digits to binary
-    h1 = f'{int(hhmmss[0][0]):02b}'
-    h2 = f'{int(hhmmss[0][1]):04b}'
-
-    # convert minute digits to binary
-    m1 = f'{int(hhmmss[1][0]):03b}'
-    m2 = f'{int(hhmmss[1][1]):04b}'
-
-    # convert second digits to binary
-    s1 = f'{int(hhmmss[2][0]):03b}'
-    s2 = f'{int(hhmmss[2][1]):04b}'
-
-    # build output string
     TO_MORSE = str.maketrans('01', '.-')
-    return f'{h1} {h2} : {m1} {m2} : {s1} {s2}'.translate(TO_MORSE)
+    fmt = "{:02b} {:04b} : {:03b} {:04b} : {:03b} {:04b}"
+    hhmmss = [number.zfill(2) for number in time_string.split(':')]
+    digits = [int(digit) for digit in ''.join(hhmmss)]
+    return fmt.format(*digits).translate(TO_MORSE)
 
 
 if __name__ == '__main__':
