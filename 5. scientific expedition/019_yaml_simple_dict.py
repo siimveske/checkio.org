9cbb501d76dc2151a44865440176047a5b472649
Siim Veske <siim.veske@gmail.com>
2022-03-19 09:36:25 +0200

    solve "YAML. Simple Dict"

--- 5. scientific expedition/019_yaml_simple_dict.py
+++ 5. scientific expedition/019_yaml_simple_dict.py
@@ -1,17 +1,17 @@
 '''https://py.checkio.org/en/mission/yaml-simple-dict/'''
 
 
-def yaml(a):
-    # your code here
-    return None
+def yaml(a: str) -> dict:
+    result = {}
+    for line in a.splitlines():
+        if line:
+            key, value = line.split(': ')
+            value = int(value) if value.isnumeric() else value
+            result[key] = value
+    return result
 
 
 if __name__ == '__main__':
-    print("Example:")
-    print(yaml("""name: Alex
-age: 12"""))
-
-    # These "asserts" are used for self-checking and not for an auto-testing
     assert yaml("""name: Alex
 age: 12""") == {'age': 12, 'name': 'Alex'}
     assert yaml("""name: Alex Fox
