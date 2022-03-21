96199bed22de60116aac0c6df5142991138bc3a4
Siim Veske <siim.veske@gmail.com>
2022-03-21 10:34:40 +0200

    refactor "Largest Histogram"

--- 7. ice base/003_largest_histogram.py
+++ 7. ice base/003_largest_histogram.py
@@ -1,31 +1,31 @@
 
 '''https://py.checkio.org/en/mission/largest-histogram/'''
 
+import random
+from helpers import timeit
+
 
 def largest_histogram(histogram):
-    cols = len(histogram)
-    rows = max(histogram) + 1
-    maximum = 0
-
-    for height in range(1, rows):
-        acc = 0
-        for col in range(cols):
-            if histogram[col] < height:
-                if acc > maximum:
-                    maximum = acc
-                acc = 0
+    sum = 0
+    for i in range(min(histogram), max(histogram) + 1):
+        sub_sum = 0
+        for n in histogram:
+            if n >= i:
+                sub_sum += i
             else:
-                acc += height
-        if acc > maximum:
-            maximum = acc
-
-    return maximum
+                sub_sum = 0
+            if sub_sum > sum:
+                sum = sub_sum
+    return sum
 
 
 if __name__ == "__main__":
+
     assert largest_histogram([5]) == 5, "one is always the biggest"
     assert largest_histogram([5, 3]) == 6, "two are smallest X 2"
     assert largest_histogram([1, 1, 4, 1]) == 4, "vertical"
     assert largest_histogram([1, 1, 3, 1]) == 4, "horizontal"
     assert largest_histogram([2, 1, 4, 5, 1, 3, 3]) == 8, "complex"
-    print("OK")
+
+    data = [random.randint(50, 500) for _ in range(10000)]
+    timeit(largest_histogram, data)
