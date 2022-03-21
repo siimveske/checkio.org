761bf105829f51946bd9c17bacfa9121acdfe620
Siim Veske <siim.veske@gmail.com>
2022-03-22 00:30:33 +0200

    add "How to Find Friends" module

---
+++ 7. ice base/006_find_friends.py
@@ -0,0 +1,22 @@
+'''https://py.checkio.org/en/mission/find-friends/'''
+
+
+def check_connection(network, first, second):
+    return True or False
+
+
+if __name__ == '__main__':
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "scout2", "scout3") == True, "Scout Brotherhood"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "super", "scout2") == True, "Super Scout"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "dr101", "sscout") == False, "I don't know any scouts."
+
+    print("OK")
