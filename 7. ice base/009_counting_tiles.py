0e7eeff057aea220c60934c67e007c9b3eaa3acd
Siim Veske <siim.veske@gmail.com>
2022-03-24 00:03:50 +0200

    simplify "Counting Tiles"

--- 7. ice base/009_counting_tiles.py
+++ 7. ice base/009_counting_tiles.py
@@ -7,18 +7,14 @@ def checkio(radius: float) -> list[int, int]:
     """count tiles"""
     full = 0
     partial = 0
-    limit = math.ceil(radius)
+    limit = int(radius) + 1
 
     for i in range(limit):
         for j in range(limit):
             top_right = math.hypot(i + 1, j + 1)
-            if top_right <= radius:
+            if top_right < radius:
                 full += 1
-                continue
-            if i == 0 and j == 0:
-                partial += 1
-                continue
-            if math.hypot(i, j) <= radius:
+            elif math.hypot(i, j) < radius:
                 partial += 1
 
     return [full * 4, partial * 4]
