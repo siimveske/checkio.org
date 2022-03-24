29672855adb812dfba3dbb52411026df64f30710
Siim Veske <siim.veske@gmail.com>
2022-03-24 22:59:49 +0200

    solve "Oil Pie"

--- 7. ice base/011_oil_pie.py
+++ 7. ice base/011_oil_pie.py
@@ -1,12 +1,21 @@
 '''https://py.checkio.org/en/mission/oil-pie/'''
+from fractions import Fraction as frac
 
 
-def divide_pie(groups):
-    return 1, 1
+def divide_pie(groups: tuple) -> tuple:
+    result = frac(1)
+    num_of_groups = sum(map(abs, groups))
+    for group_size in groups:
+        if group_size > 0:
+            result -= frac(group_size, num_of_groups)
+        else:
+            part = result / num_of_groups
+            result -= part * abs(group_size)
+
+    return result.as_integer_ratio()
 
 
 if __name__ == '__main__':
-    assert isinstance((2, -2), (tuple, list)), "Return tuple or list"
     assert tuple(divide_pie((2, -1, 3))) == (1, 18), "Example"
     assert tuple(divide_pie((1, 2, 3))) == (0, 1), "All know about the pie"
     assert tuple(divide_pie((-1, -1, -1))) == (8, 27), "One by one"
