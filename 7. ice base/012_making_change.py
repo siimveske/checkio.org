2b97e3f8c1c58e170c562dc8b63be94cb04a2754
Siim Veske <siim.veske@gmail.com>
2022-03-25 10:16:23 +0200

    save WIP

--- 7. ice base/012_making_change.py
+++ 7. ice base/012_making_change.py
@@ -1,11 +1,35 @@
 '''https://py.checkio.org/en/mission/making-change/'''
 
 
-def checkio(price: int, denominations: list[int]) -> int:
+def checkio(amount: int, coins: list[int]) -> int:
     """
         return the minimum number of coins that add up to the price
     """
-    return None
+
+    res = calculate(amount, coins, {})
+    if res == float("inf"):
+        return None
+    else:
+        return res
+
+
+def calculate(amount: int, coins: list[int], memo: dict[int, int]) -> int:
+    if amount in memo:
+        return memo[amount]
+
+    if amount == 0:
+        return 0
+    if amount < 0:
+        return float("inf")
+
+    min_cost = float("inf")
+    for coin in coins:
+        cost = 1 + calculate(amount - coin, coins, memo)
+        if cost < min_cost:
+            min_cost = cost
+
+    memo[amount] = min_cost
+    return min_cost
 
 
 if __name__ == '__main__':
