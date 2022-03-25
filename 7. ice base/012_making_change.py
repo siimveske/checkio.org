7de92529ca27b06b68ec221270de55e0406529a5
Siim Veske <siim.veske@gmail.com>
2022-03-25 09:50:44 +0200

    add "Making Change" module

---
+++ 7. ice base/012_making_change.py
@@ -0,0 +1,14 @@
+'''https://py.checkio.org/en/mission/making-change/'''
+
+
+def checkio(price: int, denominations: list[int]) -> int:
+    """
+        return the minimum number of coins that add up to the price
+    """
+    return None
+
+
+if __name__ == '__main__':
+    assert checkio(8, [1, 3, 5]) == 2
+    assert checkio(12, [1, 4, 5]) == 3
+    print('OK')
