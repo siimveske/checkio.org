4979ea848669a8a530a12c7f3cddd5d775a13732
Siim Veske <siim.veske@gmail.com>
2022-03-26 13:24:56 +0200

    solve "Identify Block"

--- 7. ice base/016_identify_block.py
+++ 7. ice base/016_identify_block.py
@@ -1,7 +1,7 @@
 '''https://py.checkio.org/en/mission/identify-block/'''
 
 
-def identify_block(numbers):
+def identify_block(numbers: set) -> str:
     """
     grid(4x4):
     +--+--+--+--+
@@ -25,6 +25,8 @@ def identify_block(numbers):
      *
 
     """
+
+    # Possible block values if positioned to top left corner
     MAPPING = {
         'I': [{1, 2, 3, 4}, {1, 5, 9, 13}],
         'J': [{2, 6, 9, 10}, {1, 2, 3, 7}, {1, 2, 5, 9}, {1, 5, 6, 7}],
@@ -34,6 +36,34 @@ def identify_block(numbers):
         'Z': [{1, 2, 6, 7}, {2, 5, 6, 9}],
         'T': [{1, 2, 3, 6}, {1, 5, 6, 9}, {2, 5, 6, 7}, {2, 5, 6, 10}]
     }
+
+    grid_size = 4
+    min_row = float('inf')
+    min_col = float('inf')
+    block_cords = []
+
+    # Calculate block coordinates
+    for i in range(grid_size):
+        for j in range(grid_size):
+            value = (i * grid_size) + (j + 1)
+            if value in numbers:
+                min_row = min(min_row, i)
+                min_col = min(min_col, j)
+                block_cords.append((i, j))
+
+    # Move block to top left corner and calculate it's values
+    value = set()
+    for r, c in block_cords:
+        new_r = r - min_row
+        new_c = c - min_col
+        new_val = (new_r * grid_size) + (new_c + 1)
+        value.add(new_val)
+
+    for letter, values in MAPPING.items():
+        if value in values:
+            return letter
+
+    return None
 
 
 if __name__ == '__main__':
