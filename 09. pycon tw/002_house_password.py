fe057d8d0a866cc205d898a0c2b1b7ff5c0190a3
Siim Veske <siim.veske@gmail.com>
2022-03-27 17:31:55 +0300

    solve "House Password"

--- 09. pycon tw/002_house_password.py
+++ 09. pycon tw/002_house_password.py
@@ -2,15 +2,30 @@
 
 
 def checkio(data: str) -> bool:
-    return True or False
+    length = 0
+    has_digits = 0
+    has_lowercase = 0
+    has_uppercase = 0
+
+    for letter in data:
+        if letter.isalpha():
+            if letter.islower():
+                has_lowercase += 1
+            else:
+                has_uppercase += 1
+        else:
+            has_digits += 1
+        length += 1
+
+    return bool(length >= 10 and has_digits and has_lowercase and has_uppercase)
 
 
 if __name__ == '__main__':
-    assert checkio('A1213pokl') == False, "1st example"
-    assert checkio('bAse730onE4') == True, "2nd example"
-    assert checkio('asasasasasasasaas') == False, "3rd example"
-    assert checkio('QWERTYqwerty') == False, "4th example"
-    assert checkio('123456123456') == False, "5th example"
-    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    assert checkio('A1213pokl') == False
+    assert checkio('bAse730onE4') == True
+    assert checkio('asasasasasasasaas') == False
+    assert checkio('QWERTYqwerty') == False
+    assert checkio('123456123456') == False
+    assert checkio('QwErTy911poqqqq') == True
 
     print("OK")
